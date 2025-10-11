b184c30717add418148e24ee2058cefde997ed11
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2025-10-11 09:15:42 +0200

    La till kontakt form, ska lägga till logik för att skicka med.

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, render_template
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+
+load_dotenv()
+
+app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+
+print("Hemlig nyckel:", app.secret_key)
+
+app.secret_key = os.getenv("SECRET_KEY")
+
+app.config['MAIL_SERVER'] = os.getenv("MAIL_SERVER")
+app.config['MAIL_PORT'] = int(os.getenv("MAIL_PORT"))
+app.config['MAIL_USE_TLS'] = os.getenv("MAIL_USE_TLS") == "True"
+app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
+app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
+
+mail = Mail(app)
 
 @app.route('/')
 def home():
