f7e7fd7a9e575b15c8aad05331e472ad6160e97e
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2025-10-12 07:35:43 +0200

    Bugfix för att se varför railway inte bygger

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 from flask import Flask, render_template
 from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
-load_dotenv()
+#load_dotenv()
 
-app.secret_key = os.getenv("SECRET_KEY")
+""" app.secret_key = os.getenv("SECRET_KEY")
 
 print("Hemlig nyckel:", app.secret_key)
 
@@ -19,7 +19,7 @@ app.config['MAIL_USE_TLS'] = os.getenv("MAIL_USE_TLS") == "True"
 app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
 app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
 
-mail = Mail(app)
+mail = Mail(app) """
 
 @app.route('/')
 def home():
