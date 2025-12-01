0cfb9de4f41b82c08b83444632fa86f3cb939a50
Filip Nordqvist <114819894+FilipNordqvist@users.noreply.github.com>
2025-12-01 19:59:18 +0100

    Removing handler bluepirint

--- routes/media_handler.py
+++ routes/media_handler.py
@@ -1,32 +1,13 @@
 import os
 import boto3
-from botocore.client import Config
 from dotenv import load_dotenv
-from flask import Blueprint
 
 # Ladda miljövariabler från .env-fil
 load_dotenv()
 
-# Skapa blueprint
-media_handler_bp = Blueprint("media_handler", __name__)
 
 # Hämta variabler från miljön
 R2_ACCESS_KEY_ID = os.environ.get("R2_ACCESS_KEY_ID")
 R2_SECRET_ACCESS_KEY = os.environ.get("R2_SECRET_ACCESS_KEY")
 R2_BUCKET_NAME = os.environ.get("R2_BUCKET_NAME")
 R2_BUCKET_ENDPOINT = os.environ.get("R2_BUCKET_ENDPOINT")
-
-# Skapa S3-klient (R2 är S3-kompatibelt)
-s3 = boto3.client(
-    "s3",
-    endpoint_url=R2_BUCKET_ENDPOINT,
-    aws_access_key_id=R2_ACCESS_KEY_ID,
-    aws_secret_access_key=R2_SECRET_ACCESS_KEY,
-    config=Config(signature_version="s3v4"),
-)
-
-
-# Ladda upp bild till R2
-def upload_image_to_r2(file_name, file_data):
-    s3.put_object(Bucket=R2_BUCKET_NAME, Key=file_name, Body=file_data)
-    print(f"Bild {file_name} uppladdad till R2!")
